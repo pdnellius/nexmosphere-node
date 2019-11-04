b7d201cc80398d6ad69db2b5ed9e85eaa78c646d
Pete Nellius <pete.nellius@akqa.com>
2019-11-04 16:16:02 -0500

    Adding some comments to the code for explanation

---
+++ README.md
@@ -0,0 +1,5 @@
+#Nexmosphere Serial Streaming
+
+This project is a simple node script for logging values coming through Nexmosphere's RS-232 to USB interface. It uses the node `serialport` package to log data stream values to the console.  
+
+Simply a good starting place for implementing Nexmosphere with node.
--- stream.js
+++ stream.js
@@ -1,6 +1,8 @@
 const SerialPort = require('serialport');
 const Readline = require('@serialport/parser-readline');
-const port = new SerialPort('/dev/tty.usbserial-FT1YVCEB', {
+// Replace the value below with the port name coming from your computer. You can use `npx @serialport/list` to find the value for this
+const portLocation = '/dev/tty.usbserial-FT1YVCEB'
+const port = new SerialPort(portLocation, {
   baudRate: 115200
 });
 const parser = port.pipe(new Readline({ delimiter: '\r\n' }));
